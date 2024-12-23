9c5447de05f2d61c2a9d65486cc15758cfda19b6
mandlaanilbabu <mandla.anilbabu@gmail.com>
2024-12-23 23:55:14 +0530

    added

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
+    bucket = "primuslearning-appanil"
     region = "us-east-1"
     key = "eks/terraform.tfstate"
   }
--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,7 +1,7 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "myapp-eks-cluster"
+    cluster_name = "myappanil-eks-cluster"
     cluster_version = "1.24"
 
     cluster_endpoint_public_access  = true
@@ -11,7 +11,7 @@ module "eks" {
 
     tags = {
         environment = "development"
-        application = "myapp"
+        application = "myappanil"
     }
 
     eks_managed_node_groups = {
